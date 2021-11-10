31f28e195165feb8ec0f0fe884dcda9836b979fa
Mehmet Ali Baykara <mehmetalibaykara@gmail.com>
2021-11-10 11:55:02 +0100

    Update setup.sh

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -8,7 +8,7 @@ mkdir ~/repos ~/tools ~/testenv
 echo "set ts=2 sw=2" > ~/.vimrc
 
 
-#install kubectl
+echo "Installing kubectl and configuring..."
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 echo "$(<kubectl.sha256) kubectl" | sha256sum --check
 echo 'source <(kubectl completion bash)' >>~/.bashrc
@@ -16,3 +16,14 @@ kubectl completion bash >/etc/bash_completion.d/kubectl
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
 
+echo "Installing flux ..."
+curl -s https://fluxcd.io/install.sh | sudo bash
+. <(flux completion bash)
+
+echo "Installing helm 3 ..."
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+echo "Installing K9S..."
+curl -sS https://webinstall.dev/k9s | bash
